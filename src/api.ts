0b66ed38c174ab4944800fd23a8edfcd40398615
alexpshkov <alexpshkov@gmail.com>
2024-10-16 03:40:20 +0300

    fix stack create

--- src/api.ts
+++ src/api.ts
@@ -38,7 +38,7 @@ export class PortainerApi {
   }
 
   async createStack(params: CreateStackParams, body: CreateStackBody): Promise<void> {
-    await this.axiosInstance.post('/stacks', body, { params })
+    await this.axiosInstance.post('/stacks/create/standalone/file', body, { params })
   }
 
   async updateStack(id: number, params: UpdateStackParams, body: UpdateStackBody): Promise<void> {
